3a1693417f3fb440e9e036e194cc33f65b10269d
maxamed1st <maxamed1st@icloud.com>
2023-09-09 02:02:26 +0100

    initial setup for the header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,18 @@ module.exports = {
         sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
         montserrat: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif']
       },
+      colors: {
+        base1: "hsl(140, 10%, 10%)",
+        base1fg: "hsl(0, 0%, 85%)",
+        base2: "hsl(140, 10%, 12%)",
+        base3: "hsl(140, 13%, 13%)",
+        neutral: "hsl(140, 10%, 11%)",
+        ghost: "hsl(0, 0%, 50%)",
+        accent: "hsl(37, 67%, 58%)",
+        accent2: "hsl(40, 67%, 40%)",
+        success: "hsl(100, 100%, 50%)",
+        herogradient: "gr"
+      },
     },
   },
 	plugins: [],
